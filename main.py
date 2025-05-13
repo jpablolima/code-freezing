3965678543396643bee5c2929257ba53921f97a0
João Pablo <jp.pablolima@gmail.com>
2025-05-13 07:11:52 -0300

    configuração de logging

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import logging
 import sys
 import yaml
 
 CONFIG_FILE = "config.yml"
+logging.basicConfig(
+    format="[%(asctime)s] [%(levelname)s] - %(message)s",
+    datefmt="%d-%m-%Y %I:%M:%S",
+    level=logging.DEBUG
+)
+
 
 def get_config(filename:str) -> dict:
     with open("config.yml") as f:
@@ -13,7 +20,7 @@ def upack_config(config:dict) -> tuple:
         bypass_group = config["bypass_group"]
         freezing_dates = config["freezing_dates"]
     except KeyError:
-        print(f"One  of the fields are not present: ' bypass_group' of 'freezing_dates' on config file{CONFIG_FILE}")
+        logging.error(f"One  of the fields are not present: ' bypass_group' of 'freezing_dates' on config file{CONFIG_FILE}")
         sys.exit(1)
     return(bypass_group, freezing_dates)
 
@@ -21,7 +28,7 @@ def main():
     config = get_config(CONFIG_FILE)
     bypass_group, freezing_dates = upack_config(config)
 
-    print(bypass_group)
+    logging.info(bypass_group)
 
 
 if __name__== "__main__":
