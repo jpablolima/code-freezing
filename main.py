df80cb92117d9b5516ed9e556fc37bd332ace3ea
João Pablo <jp.pablolima@gmail.com>
2025-05-14 06:47:12 -0300

    Validação de bypass

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import logging
+import os 
 import sys
+
 import yaml
 
 CONFIG_FILE = "config.yml"
+GITLAB_USER_LOGIN=os.getenv("GITLAB_USER_LOGIN")
+
 logging.basicConfig(
     format="[%(asctime)s] [%(levelname)s] - %(message)s",
     datefmt="%d-%m-%Y %I:%M:%S",
@@ -24,11 +28,20 @@ def upack_config(config:dict) -> tuple:
         sys.exit(1)
     return(bypass_group, freezing_dates)
 
+def is_user_in_bypass_group(username: str, bypass_group: list) -> bool:
+    return username in bypass_group
+
+
 def main():
     config = get_config(CONFIG_FILE)
     bypass_group, freezing_dates = upack_config(config)
 
-    logging.info(bypass_group)
+    if is_user_in_bypass_group(GITLAB_USER_LOGIN, bypass_group):
+        logging.info(f"{GITLAB_USER_LOGIN} is in bypass group, exitting")
+        sys.exit(0)
+    
+
+
 
 
 if __name__== "__main__":
